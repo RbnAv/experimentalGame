865299433bdee073bd7aab16e61d6f0b82aae32a
JesusFerZamo <jfzmora@gmail.com>
2018-05-14 19:23:17 +0200

    Añadido funcionalidad al boton continuar para que pase a la siguiente pantalla.

--- experimentalGame/src/experimentalGame/game/view/MainUIController.java
+++ experimentalGame/src/experimentalGame/game/view/MainUIController.java
@@ -91,6 +91,29 @@ public class MainUIController implements Initializable {
 			// TODO: handle exception
 		}
 	}
+	
+	public void continuar(ActionEvent event) {
+		try{
+			 
+			//Te lo comento para que  lo comprendamos tú y yo
+			//Léeme el source del archivo que te digo fxml y te pongo el path
+			 
+			FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("MainUI.fxml"));
+			Parent root1= (Parent)fxmlLoader.load();
+			 
+			//Creame un nuevo Stage o para que nos entendamos una nueva ventana windows vacía
+			Stage stage= new Stage();
+			 
+			//Y ahora dentro del Stage me metes la escena que anteriormente hemos leído y metido en root1
+			stage.setScene(new Scene(root1));
+			 
+			// Y ahora le digo que me muestres el stage
+			stage.show();
+			}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 
 	/**
 	 * Is called by the main application to give a reference back to itself.
