4ad21ef447a0ba7a341d5cc0cc51600a60cf2940
RbvAv <actingasaparticle@gmail.com>
2018-05-27 14:01:23 +0200

    ventana combate y controlador

---
+++ experimentalGame/src/experimentalGame/game/view/BattleController.java
@@ -0,0 +1,52 @@
+package experimentalGame.game.view;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class BattleController {
+
+	@FXML
+	private Button ataque1 = new Button();
+	@FXML
+	private Button ataque2 = new Button();
+	@FXML
+	private Button botiquin = new Button();
+	@FXML
+	private Button granada = new Button();
+	@FXML
+	private Button habilidad1 = new Button();
+	@FXML
+	private Button habilidad2 = new Button();
+	@FXML
+	private Button habilidad3 = new Button();
+
+	public BattleController() {
+
+	}
+
+	public void iniciarCombate() {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("battle.fxml"));
+		Parent root;
+		try {
+			root = (Parent) fxmlLoader.load();
+
+			Stage stage = new Stage();
+
+			stage.setScene(new Scene(root));
+
+			stage.show();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+	
+
+}
